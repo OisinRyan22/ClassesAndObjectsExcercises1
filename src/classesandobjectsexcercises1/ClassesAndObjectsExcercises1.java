b93f6587143564a339990cd8b2840e88c93e9fc7
OisinRyan22 <sba23149@student.cct.ie>
2023-12-03 16:31:57 +0000

    Update ClassesAndObjectsExcercises1.java

--- src/classesandobjectsexcercises1/ClassesAndObjectsExcercises1.java
+++ src/classesandobjectsexcercises1/ClassesAndObjectsExcercises1.java
@@ -14,7 +14,7 @@ public class ClassesAndObjectsExcercises1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       System.out.println("Hello World");
+      System.out.println("Hello world");
     }
     
 }
