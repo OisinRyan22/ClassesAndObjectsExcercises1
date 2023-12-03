792bc45a4828267a55512cc547a11b810c8829db
OisinRyan22 <sba23149@student.cct.ie>
2023-12-03 16:32:47 +0000

    Hello Class

--- src/classesandobjectsexcercises1/ClassesAndObjectsExcercises1.java
+++ src/classesandobjectsexcercises1/ClassesAndObjectsExcercises1.java
@@ -15,6 +15,7 @@ public class ClassesAndObjectsExcercises1 {
      */
     public static void main(String[] args) {
       System.out.println("Hello world");
+      System.out.println("Hello class");
     }
     
 }
