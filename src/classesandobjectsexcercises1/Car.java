22098018e576334a1c447cd39ace2b8d1113d990
OisinRyan22 <sba23149@student.cct.ie>
2023-12-03 16:44:23 +0000

    Drive method

--- src/classesandobjectsexcercises1/Car.java
+++ src/classesandobjectsexcercises1/Car.java
@@ -13,4 +13,9 @@ public class Car {
     String model;
     int numWheels;
     String colour;
+    
+    public void drive(String direction) {
+        System.out.println("The " + colour + " " + make + " is driving " + direction);
+         
+    }
 }
